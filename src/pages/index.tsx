4f16a03f62b12554964fbca8494d89089db81542
daichipyon <daichidaze@gmai.com>
2023-05-05 22:01:59 +0900

    修正

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,28 +6,31 @@ type Post = {
   body: string
 }
 
-// ユーザー一覧
+type Props = {
+  posts: Post[]
+}
+
+// 投稿一覧
 export default function Home(props: { posts: Post[] }) {
-  console.log(props.posts)
   return (
     <>
-    <h1>ユーザー一覧</h1>
-    <ul> 
-      {/* {props.posts.map((post) => (
-        <li key={post.userId}>
-          <h2>{post.title}</h2>
-          <p>{post.body}</p>
-        </li>
-      ))} */}
-    </ul>
+      <h1>投稿一覧</h1>
+      <ul>
+        {props.posts.map((post) => (
+          <li key={post.userId}>
+            <h2>{post.title}</h2>
+            <p>{post.body}</p>
+          </li>
+        ))}
+
+      </ul>
     </>
   )
 }
 
- export const getServerSideProps: GetServerSideProps = async () => {
+export const getServerSideProps: GetServerSideProps<Props> = async () => {
   const res = await fetch('https://jsonplaceholder.typicode.com/posts')
   const posts: Post[] = await res.json()
-
   return {
     props: {
       posts
